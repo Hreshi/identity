757c7f774a0e7bf4c227d776dba5e24337a2ba49
hreshi <hkale746@gmail.com>
2023-08-09 13:45:17 +0530

    Added contact entity

--- src/main/java/com/hreshi/identity/Contact.java
+++ src/main/java/com/hreshi/identity/Contact.java
@@ -10,6 +10,38 @@ import jakarta.persistence.Id;
 @Entity
 public class Contact {
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + id;
+        result = prime * result + ((phoneNumber == null) ? 0 : phoneNumber.hashCode());
+        result = prime * result + ((email == null) ? 0 : email.hashCode());
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Contact other = (Contact) obj;
+        // if (id != other.id)
+        //     return false;
+        if (phoneNumber == null) {
+            if (other.phoneNumber != null)
+                return false;
+        } else if (!phoneNumber.equals(other.phoneNumber))
+            return false;
+        if (email == null) {
+            if (other.email != null)
+                return false;
+        } else if (!email.equals(other.email))
+            return false;
+        return true;
+    }
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
@@ -75,6 +107,9 @@ public class Contact {
     }
     public void setDeletedAt(LocalDateTime deletedAt) {
         this.deletedAt = deletedAt;
+    }
+    public boolean isPrimary() {
+        return "primary".equals(linkPrecedence);
     }
 
 }
