70b65c05b3b53eaf4e87b9946a50c085c5973133
hreshi <hkale746@gmail.com>
2023-08-09 13:45:41 +0530

    Added contact repo for crud

--- src/main/java/com/hreshi/identity/ContactRepository.java
+++ src/main/java/com/hreshi/identity/ContactRepository.java
@@ -8,4 +8,11 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ContactRepository extends CrudRepository<Contact, Integer>{
     List<Contact> findAll();
+
+    Contact findByEmail(String email);
+
+    Contact findByPhoneNumber(String phoneNumber);
+
+    List<Contact> findAllByLinkedId(int id);
+
 }
